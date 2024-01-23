402aeed40d63c961b25523eef857e0393ecca76a
Horizon719 <milichovszky.sandor@gmail.com>
2024-01-23 10:32:11 +0100

    Topic model

---
+++ backend/src/main/java/hu/kaoszkviz/server/api/model/Topic.java
@@ -0,0 +1,34 @@
+package hu.kaoszkviz.server.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = Topic.TABLE_NAME)
+public class Topic {
+    
+    @Id
+    @Setter
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Setter
+    @Getter
+    @Column(nullable = false, columnDefinition = "nvarchar(20)")
+    private String title;
+    
+    public static final String TABLE_NAME = "topic";
+}
