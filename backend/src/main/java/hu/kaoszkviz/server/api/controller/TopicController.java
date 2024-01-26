d0479cbb2cd9e6fe8ed60c5f3cbe27e22762eb78
Horizon719 <milichovszky.sandor@gmail.com>
2024-01-26 12:36:10 +0100

    Topic végpontok

---
+++ backend/src/main/java/hu/kaoszkviz/server/api/controller/TopicController.java
@@ -0,0 +1,37 @@
+package hu.kaoszkviz.server.api.controller;
+
+import hu.kaoszkviz.server.api.model.Topic;
+import hu.kaoszkviz.server.api.service.TopicService;
+import java.util.HashMap;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/topic")
+public class TopicController {
+    
+    @Autowired
+    private TopicService topicService;
+    
+    @GetMapping(name = "")
+    public List<Topic> getTopics(@RequestBody HashMap<String, String> requestBody){
+        return this.topicService.getTopics(requestBody.get("searchString"));
+    }
+    
+    @PostMapping("")
+    public ResponseEntity<String> addTopic(@RequestBody Topic topic) {
+        return this.topicService.addTopic(topic);
+    }
+    
+    @DeleteMapping("")
+    public void deleteById(@RequestBody HashMap<String, String> requestBody){
+        this.topicService.deleteById(Long.parseLong(requestBody.get("id")));
+    }
+}
--- backend/src/main/java/hu/kaoszkviz/server/api/service/TopicService.java
+++ backend/src/main/java/hu/kaoszkviz/server/api/service/TopicService.java
@@ -1,12 +1,41 @@
 package hu.kaoszkviz.server.api.service;
 
+import hu.kaoszkviz.server.api.model.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import hu.kaoszkviz.server.api.repository.TopicRepository;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Service
 public class TopicService {
     
     @Autowired
     private TopicRepository topicRepository;
+    
+    public List<Topic> getTopics(String txt){
+        if (txt == null) {
+            return this.topicRepository.findAll();
+        } else {
+            return this.topicRepository.searchBy(txt);
+        }
+    }
+    
+    public ResponseEntity<String> deleteById(Long id){
+        this.topicRepository.deleteById(id);
+        if (this.topicRepository.findById(id).isEmpty()) {
+            return new ResponseEntity<>("failed", HttpStatus.BAD_REQUEST);
+        } else{
+            return new ResponseEntity<>("ok", HttpStatus.OK);
+        }
+    }
+    
+    public ResponseEntity<String> addTopic(Topic topic) {
+        if (this.topicRepository.save(topic) == null) {
+            return new ResponseEntity<>("failed", HttpStatus.BAD_REQUEST);
+        } else{
+            return new ResponseEntity<>("ok", HttpStatus.CREATED);
+        }
+    }
 }
