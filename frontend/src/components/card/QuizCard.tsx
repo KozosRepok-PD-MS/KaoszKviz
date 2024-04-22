3d275b4bb203d23766bba74629b09d5e9891e9cc
Horizon719 <milichovszky.sandor@gmail.com>
2024-04-22 19:44:12 +0200

    QuestionList refaktor (itt lehet kvíz módosítani és törölni)

--- frontend/src/components/card/QuizCard.tsx
+++ frontend/src/components/card/QuizCard.tsx
@@ -1,57 +1,22 @@
-import React, { useContext, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Quiz } from "../../model/Quiz";
 import "./QuizCard.css";
 import Button from "../buttons/Button";
-import { AuthContext } from "src/context/AuthContext";
-import CreateQuiz from "src/pages/CreateQuiz";
-
 
 export type QuizCardProps = {
     quiz: Quiz
 }
 
 const QuizCard: React.FC<QuizCardProps> = (props: QuizCardProps) => {
-    const{auth} = useContext(AuthContext);
-    const authUserId = auth?.user?.id;
-    const [isEditing, setIsEditing] = useState(false);
-    const [editState, setEditState] = useState(<></>);
-
     let questionsLink: string = "/quiz/" + props.quiz.id;
 
-    function handleEdit(){
-        if (isEditing) {
-            setEditState(<CreateQuiz quizId={-1n} isnew={false}/>);
-        } else {
-            setEditState(<></>);
-        }
-        setIsEditing(!isEditing);
-    }
-    
     return(
         <div className="quizCard">
             <div className="quizCardName">{props.quiz.title}</div>
             <div className="quizCardDescription">{props.quiz.description}</div>
             <div className="quizCardImg"><img src="/logo512.png"/></div>
             <div className="quizCardStart"><Button name="startButton" title="START" type="button"/></div>
-            {
-                authUserId === props.quiz.ownerId ? 
-                    <div>
-                        <div className="quizCardEdit">
-                            {isEditing ? 
-                                <Button name="editButton" title="BACK" type="button" onClickFn={handleEdit}/>
-                                :
-                                <Button name="editButton" title="EDIT" type="button" onClickFn={handleEdit}/>}
-                            
-                            {editState}
-                        </div>
-                        <div className="quizCardDelete">
-                            <Button name="deleteButton" title="DELETE" type="button"/>
-                        </div>
-                    </div> 
-                    : 
-                    ""
-            }       
             <div><Link className="" to={questionsLink} style={{ textDecoration: 'none' }}>Kvíz részletei</Link></div>
             
         </div>
--- frontend/src/pages/QuestionList.tsx
+++ frontend/src/pages/QuestionList.tsx
@@ -1,31 +1,61 @@
 
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { TQuestionList, Question } from "../model/Question";
 import { AxiosResponse } from "axios";
 import ApiHandler, { ApiError } from "../helper/ApiHandler";
 import { API_CONTROLLER } from "../config/ApiEndpoints";
 import QuestionCard from "../components/card/QuestionCard";
 import { useParams } from "react-router";
+import { AuthContext } from "src/context/AuthContext";
+import CreateQuiz from "./CreateQuiz";
+import Button from "src/components/buttons/Button";
+import { Quiz } from "src/model/Quiz";
+import "./QuestionList.css";
 
 type QuestionProps = {}
 
 
 const QuestionList: React.FC = (props: QuestionProps) => {
+    const{auth} = useContext(AuthContext);
+    const {id: quizId} = useParams();
+    const authUserId = auth?.user?.id;
+    const [isEditing, setIsEditing] = useState(false);
+    const [editState, setEditState] = useState(<></>);
     const[questions, setQustions] = useState<TQuestionList>( {} as TQuestionList );
-    const {id} = useParams();
+    const[quiz, setQuiz] = useState<Quiz>({} as Quiz);
 
-    function callbackFn(response: AxiosResponse<any, any>) {
+    function handleEdit(){
+        if (isEditing) {
+            setEditState(<CreateQuiz quizId={BigInt(quizId!)} isnew={false}/>);
+        } else {
+            setEditState(<></>);
+        }
+        setIsEditing(!isEditing);
+    }
+
+    function questionCallbackFn(response: AxiosResponse<any, any>) {
         setQustions(response.data as TQuestionList);
     }
     
+    function quizCallbackFn(response: AxiosResponse<any, any>) {
+        setQuiz(response.data as Quiz);
+    }
+
     useEffect(() => {
         try {
-            ApiHandler.executeApiCall(API_CONTROLLER.QUESTION, "getQuestionsByQuizId", callbackFn, undefined, new Map([
+            ApiHandler.executeApiCall(API_CONTROLLER.QUESTION, "getQuestionsByQuizId", questionCallbackFn, undefined, new Map([
                 [
                     "quizId",
-                    id!.toString()
+                    quizId!.toString()
                 ]
-            ]),);            
+            ]),);
+            
+            ApiHandler.executeApiCall(API_CONTROLLER.QUIZ, "getQuizById", quizCallbackFn, undefined, new Map([
+                [
+                    "id",
+                    quizId!.toString()
+                ]
+            ]),);
         } catch (error) {
             const ERR: ApiError = error as ApiError;            
             alert(ERR.getMessage);
@@ -34,16 +64,42 @@ const QuestionList: React.FC = (props: QuestionProps) => {
 
     return(
         <div className="content">
-            {
-                questions.length > 0 ?
-                    questions.map((question: Question, key: number) => {
-                        return(
-                            <QuestionCard question={question} key={key}></QuestionCard>
-                        )
-                    })
-                :
-                    "Ennek a kvíznek nincs kérdése"
-            }
+            <div className="questionList">
+                <div className="quizDatas">
+                    <div className="quizCardName">{quiz.title}cím.....</div>
+                    <div className="quizCardImg"><img src="/logo512.png"/></div>
+                    <div className="quizCardDescription">{quiz.description}leírás......................................</div>
+                    {
+                        authUserId === quiz.ownerId ? 
+                            <div>
+                                <div className="quizCardEdit">
+                                    {isEditing ? 
+                                        <Button name="editButton" title="BACK" type="button" onClickFn={handleEdit}/>
+                                        :
+                                        <Button name="editButton" title="EDIT" type="button" onClickFn={handleEdit}/>}
+                                    {editState}
+                                </div>
+                                <div className="quizCardDelete">
+                                    <Button name="deleteButton" title="DELETE" type="button"/>
+                                </div>
+                            </div> 
+                            : 
+                            ""
+                    }
+                </div>
+                <div className="questions">
+                    {
+                        questions.length > 0 ?
+                            questions.map((question: Question, key: number) => {
+                                return(
+                                    <QuestionCard question={question} key={key}></QuestionCard>
+                                )
+                            })
+                        :
+                            "Ennek a kvíznek nincs kérdése"
+                    }
+                </div>
+            </div>
         </div>
     )
 }
