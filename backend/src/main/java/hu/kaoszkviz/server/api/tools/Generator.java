8e6412c353564732bb02002c1cb811e00655a6b5
PetiDominik <petidomi05@gmail.com>
2024-04-29 15:09:04 +0200

    Random valamiért nem működött dockerben

--- backend/src/main/java/hu/kaoszkviz/server/api/tools/Generator.java
+++ backend/src/main/java/hu/kaoszkviz/server/api/tools/Generator.java
@@ -6,20 +6,22 @@ import hu.kaoszkviz.server.api.model.Media;
 import hu.kaoszkviz.server.api.model.User;
 import hu.kaoszkviz.server.api.repository.UserRepository;
 import java.util.List;
-import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class Generator {
-    private static final Random RND = new Random();
-    
+
     @Autowired
     private UserRepository userRepository;
     
     public Media getRandomProfilePicture() {
         User rootUser = this.userRepository.findById(0l).orElseThrow(() -> new NotFoundException(User.class, 0));
         List<Media> medias = rootUser.getMedias();
-        int randomIndex = RND.nextInt(0, medias.size());
+        int randomIndex = this.getRandom(0, medias.size());
         return medias.get(randomIndex);
+    }
+
+    private int getRandom(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
     }
 }
